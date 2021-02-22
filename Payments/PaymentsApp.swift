a20b7240332b0f76261891aeed02e0b2279e9b7e
Konstantin <constantin.l.o.p@gmail.com>
2021-02-22 11:53:24 +0700

    Initial Commit

---
+++ Payments/PaymentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaymentsApp.swift
+//  Payments
+//
+//  Created by Константин Лопаткин on 22.02.2021.
+//
+
+import SwiftUI
+
+@main
+struct PaymentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
